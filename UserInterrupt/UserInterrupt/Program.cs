805316612e5cfdce05a7a12c10401edab1f4e4ab
dryanakiev <dyanakiev@codingburgas.bg>
2024-12-04 15:15:33 +0200

    Add daemon property example

---
+++ UserInterrupt/UserInterrupt/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace UserInterrupt;
+
+class Something
+{
+    public void DoWork()
+    {
+        while (true)
+        {
+            Console.WriteLine($"{Thread.CurrentThread.Name}: Working...");
+            Thread.Sleep(1000);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Thread.CurrentThread.Name = "Main Thread";
+
+        Console.WriteLine(Thread.CurrentThread.Name);
+
+        Thread somethingThread = new Thread(() => new Something().DoWork());
+        
+        somethingThread.Name = "Something Thread";
+        
+        // Set somethingThread as a daemon property
+        somethingThread.IsBackground = true;
+        
+        somethingThread.Start();
+
+        while (somethingThread.IsAlive)
+        {
+        }
+    }
+}
